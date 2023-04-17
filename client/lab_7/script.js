af1b1713269eaa2f24076f284a108196cb1fd616
Sana Choudhry <choudhrysana300@gmail.com>
2023-04-16 23:09:12 -0400

    add initmap

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -34,6 +34,14 @@ function getRandomIntInclusive(min, max) {
       return list[index]
     })
   }
+
+  function initMap() {
+    const carto = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+  }
   
   async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
@@ -44,6 +52,8 @@ function getRandomIntInclusive(min, max) {
 
     const loadAnimation = document.querySelector('#data_load_animation');
     loadAnimation.style.display = 'none';
+
+    initMap()
 
     let currentList = [];
   
@@ -85,56 +95,10 @@ function getRandomIntInclusive(min, max) {
         console.log(newList);
         injectHTML(newList);
     })
-  
-      /*
-        ## GET requests and Javascript
-          We would like to send our GET request so we can control what we do with the results
-          But this blocks us sending a query string by default - ?resto='' won't exist
-          Let's get those form results before sending off our GET request using the Fetch API
-      */
-  
-      // this is the preferred way to handle form data in JS in 2022
-     
-  
-      // You can also access all forms in a document by using the document.forms collection
-      // But this will retrieve ALL forms, not just the one that "heard" a submit event - less good
-  
-      /*
-        ## Retrieving information from an API
-          The Fetch API is relatively new,
-          and is much more convenient than previous data handling methods.
-          Here we make a basic GET request to the server using the Fetch method
-          to send a request to the routes defined in /server/routes/foodServiceRoutes.js
-        // this is a basic GET request
-        // It does not include any of your form values, though
-      */
-  
-      
-      /*
-     ## Get request with query parameters
-        const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
-        The above request uses "string interpolation" to include an encoded version of your form values
-        It works because it has a ? in the string
-        Replace line 37 with it, and try it with a / instead to see what your server console says
-        You can check what you sent to your server in your GET request
-        By opening the "network" tab in your browser developer tools and looking at the "name" column
-        This will also show you how long it takes a request to resolve
-      */
-  
-      // This changes the response from the GET into data we can use - an "object"
+
       const arrayFromJson = await results.json();
       console.table(arrayFromJson.data); // this is called "dot notation"
-      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-      // it initially contains all 1,000 records from your request
+     
     };
   
-    
-  
-    
-  
-  /*
-    This adds an event listener that fires our main event only once our page elements have loaded
-    The use of the async keyword means we can "await" events before continuing in our scripts
-    In this case, we load some data when the form has submitted
-  */
   document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
